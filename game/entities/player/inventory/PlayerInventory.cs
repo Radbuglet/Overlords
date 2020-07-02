4ebc76b1f1ac046443f6d29d1bea0ef0a2ade431
Riley Denoyer <radbuglet@gmail.com>
2020-07-01 17:01:11 -0700

    Add networking to item stack moving

--- game/entities/player/inventory/PlayerInventory.cs
+++ game/entities/player/inventory/PlayerInventory.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
 using Godot;
 using Godot.Collections;
+using Overlords.game.definitions;
+using Overlords.helpers.csharp;
 using Overlords.helpers.network;
+using Overlords.helpers.tree;
 
 namespace Overlords.game.entities.player.inventory
 {
@@ -9,29 +12,46 @@ namespace Overlords.game.entities.player.inventory
     {
         [Export] private int _size;
         [Export] private PackedScene _stackPrefab;
-        private ItemStackRoot[] _items;
+        private ItemStackRoot[] _stacks;
         
         // Scene tree garbage
         private PlayerRoot PlayerRoot => GetParent<PlayerRoot>();
         
         public override void _Ready()
         {
-            _items = new ItemStackRoot[_size];
+            _stacks = new ItemStackRoot[_size];
         }
         
-        // Interaction methods  TODO: Implement on server and replicate
+        // Interaction methods
+        public bool IsValidSlot(int slot)
+        {
+            return _stacks.HasIndex(slot);
+        }
+
+        public ItemStackRoot GetStack(int slot)
+        {
+            return _stacks.TryGetValue(slot, out var value) ? value : null;
+        }
+        
         public IEnumerable<(int, ItemStackRoot)> GetStacks(bool filterEmpty)
         {
             for (var i = 0; i < _size; i++)
             {
-                var stack = _items[i];
+                var stack = _stacks[i];
                 if (filterEmpty && stack == null)
                     continue;
                 yield return (i, stack);
             }
         }
-        
+
         // Networking
+        private void MakeStack(int slot)
+        {
+            var stack = (ItemStackRoot) _stackPrefab.Instance();
+            stack.Name = slot.ToString();
+            AddChild(stack);
+        }
+        
         public CatchupState CatchupOverNetwork(int peerId)
         {
             var allocateStacks = new Array();
@@ -55,13 +75,71 @@ namespace Overlords.game.entities.player.inventory
                 if (!(slotRaw is int slot))
                     throw new InvalidCatchupException("Failed to pre-allocate stacks: slot was not an integer.");
 
-                if (_items[slot] != null)
+                if (_stacks[slot] != null)
                     throw new InvalidCatchupException("Failed to pre-allocate stacks: slot already has an item.");
-                
-                var stack = _stackPrefab.Instance();
-                stack.Name = slot.ToString();
-                AddChild(stack);
+
+                MakeStack(slot);
             }
+        }
+
+        [Puppet]
+        private void AddStack(int targetSlot, Dictionary catchupInfo)
+        {
+            if (!IsValidSlot(targetSlot))
+            {
+                GD.PushWarning($"Failed to {nameof(AddStack)}: invalid {nameof(targetSlot)} index.");
+                return;
+            }
+            
+            if (_stacks[targetSlot] != null)
+            {
+                GD.PushWarning($"Failed to {nameof(AddStack)}: {nameof(targetSlot)} is not empty.");
+                return;
+            }
+            MakeStack(targetSlot);
+            this.GetWorldRoot().LoginHandler.ApplyCatchupInfo(catchupInfo);
+        }
+        
+        [Puppet]
+        private void RemoveStack(int targetSlot)
+        {
+            var stack = _stacks[targetSlot];
+            if (stack == null)
+            {
+                GD.PushWarning($"Failed to {nameof(RemoveStack)}: {nameof(targetSlot)} is empty.");
+                return;
+            }
+            stack.Purge();
+            _stacks[targetSlot] = null;
+        }
+
+        [Puppet]
+        private void UpdateStackSlot(int currentSlot, int newSlot)
+        {
+            // Validate origin and target
+            var stack = _stacks[currentSlot];
+            if (stack == null)
+            {
+                GD.PushWarning($"Failed to {nameof(UpdateStackSlot)}: {nameof(currentSlot)} is empty.");
+                return;
+            }
+            
+            if (!IsValidSlot(newSlot))
+            {
+                GD.PushWarning($"Failed to {nameof(UpdateStackSlot)}: invalid {nameof(newSlot)} index.");
+                return;
+            }
+            
+            if (_stacks[newSlot] != null)
+            {
+                GD.PushWarning($"Failed to {nameof(UpdateStackSlot)}: {nameof(newSlot)} is not empty.");
+                return;
+            }
+            
+            // Move!
+            stack.Name = newSlot.ToString();
+            _stacks[newSlot] = stack;
+            _stacks[currentSlot] = null;
         }
     }
 }
